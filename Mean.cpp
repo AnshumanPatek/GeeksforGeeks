31d1d2f5e6ccffd90cabe11c8169d15bf6213aaa
AnshumanPatek <78948391+AnshumanPatek@users.noreply.github.com>
2023-02-01 02:08:02 +0530

    Create Mean.cpp

---
+++ Mean.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int mean(int N , int A[]) {
+        // code here
+     int c;
+     int sum;
+      for(int i = 0; i<N ; i++){
+      sum = sum+A[i];
+          
+      };
+      c = sum/N;
+      return c;
+
+    }
+};
