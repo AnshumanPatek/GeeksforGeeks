ec0956b309fbdceac290858d1005545b72c35890
AnshumanPatek <78948391+AnshumanPatek@users.noreply.github.com>
2023-02-11 21:16:23 +0530

    Create Two Dimensional World.java

---
+++ Two Dimensional World.java
@@ -0,0 +1,14 @@
+void twoDimensional(int*(*arr),int N){
+    
+    // Loop to iterate through matrix
+    for(int i = 0;i<N;i++){
+        for(int j = 0;j<N;j++){
+            
+            // Your code here
+            cout<<arr[i][j]<<" ";
+            
+        }
+        cout << endl;
+    }
+    
+}
