616a029e432d5eb93b37de786a97541e5c437488
AnshumanPatek <78948391+AnshumanPatek@users.noreply.github.com>
2023-02-14 16:32:08 +0530

    Create The AND Gate.java

---
+++ The AND Gate.java
@@ -0,0 +1,12 @@
+class Solution{
+    static int andGate(int arr[] , int N){
+        //code here
+        for (int i =0; i<N;i++){
+            if(arr[i]==0){
+                return 0;
+            }
+            
+        }
+        return 1;
+    }
+}
