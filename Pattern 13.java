45e8bfc63c838c34fc448765edd888ff371489c8
AnshumanPatek <78948391+AnshumanPatek@users.noreply.github.com>
2023-03-12 11:57:16 +0530

    Create Pattern 13.java

---
+++ Pattern 13.java
@@ -0,0 +1,22 @@
+
+class Solution {
+
+    void printTriangle(int n) {
+        
+        int num = 1;
+        
+        for (int i = 1; i<=n;i++){
+            for (int j = 0;j<i;j++){
+                System. out. print(num++ +" ");
+                
+            }
+            System. out. println();
+        }
+        
+        
+        
+        
+        
+        
+    }
+}
