df7f5351d3b0644650ead17c89bf9827c44481d7
AnshumanPatek <78948391+AnshumanPatek@users.noreply.github.com>
2023-03-27 22:49:41 +0530

    Create Print GFG n times.java

---
+++ Print GFG n times.java
@@ -0,0 +1,14 @@
+
+class Solution {
+
+    void printGfg(int N) {
+        // code here
+          if(N == 0)
+
+        return;
+
+        System.out.print("GFG" + " ");
+
+        printGfg(N-1);
+    }
+}
