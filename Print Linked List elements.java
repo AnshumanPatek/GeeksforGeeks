05c8bbd289cb75f003817918d9bda953d69979fc
AnshumanPatek <78948391+AnshumanPatek@users.noreply.github.com>
2023-04-21 20:38:21 +0530

    Create Print Linked List elements.java

---
+++ Print Linked List elements.java
@@ -0,0 +1,16 @@
+class Solution
+{
+    // Print elements of a linked list on console
+    // head pointer input could be NULL as well
+    // for empty list
+    void display(Node head)
+    {
+        //add code here.
+       // int count =0;
+     while(head!=NULL){
+         head = head->next;
+         head->data;
+         //count++;
+     }
+    }
+}
