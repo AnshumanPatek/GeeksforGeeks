3d610670937e20a6397134c097c2af2c84e8b1cb
AnshumanPatek <78948391+AnshumanPatek@users.noreply.github.com>
2023-05-06 10:50:01 +0530

    Create Move Last Element to Front of a Linked List.cpp

---
+++ LinkedList/Move Last Element to Front of a Linked List.cpp
@@ -0,0 +1,41 @@
+
+class Solution{
+public:
+    ListNode *moveToFront(ListNode *head){
+         if(head == NULL || head->next == NULL){
+
+            
+
+                return head;
+
+        }
+
+        
+
+        ListNode*pre = NULL;
+
+        ListNode*curr = head;
+
+        
+
+        while(curr->next != NULL){
+
+            
+
+             pre = curr;
+
+             curr = curr->next;
+
+        }
+
+        
+
+        curr->next = head;
+
+        pre->next = NULL;
+
+        
+
+        return curr;
+    }
+};
