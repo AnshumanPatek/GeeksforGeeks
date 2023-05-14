a6204f0672b90e0c19488ea4a2b19205ec35ea52
AnshumanPatek <78948391+AnshumanPatek@users.noreply.github.com>
2023-05-14 17:15:36 +0530

    Create Implement stack using array.cpp

---
+++ Stack/Implement stack using array.cpp
@@ -0,0 +1,22 @@
+void MyStack :: push(int x)
+{
+    // Your Code
+        top++;
+        arr[top]=x;
+    
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    // Your Code  
+    if(top == -1){
+        return -1;
+        }
+    else{
+        int popval = arr[top];
+        arr[top]==0;
+        top--;
+        return popval;
+    }
+}
